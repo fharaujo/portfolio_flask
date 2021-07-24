949b88cfa997657e70999a2d6496509b27544af7
fharaujo <faraujodeveloper@hotmail.com>
2021-07-24 11:24:15 -0300

    final project

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request 
+
+import smtplib
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def send():
+    if request.method == 'POST':
+        subject = request.form['subject']
+        name =  request.form['name']
+        email = request.form['email']
+        text =  request.form['message']
+        
+        # recebendo a mensagem enviado 
+        msg = f'''Subject: {subject}\n\nNome: {name}\nEmail:({email})\n\n Mensagem:\n\n{text}'''
+        
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login("araujofabio2012@gmail.com", os.getenv("PASSWORD"))
+        server.sendmail(email,"araujofabio2012@gmail.com", msg.encode('utf-8'))
+        server.quit() 
+        
+        # envio automático de confirmação 
+        thanks = f'Confirmação de email recebido.\nObrigado por entrar em contato, {name}. \n\n'
+        
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login("araujofabio2012@gmail.com", os.getenv("PASSWORD"))
+        server.sendmail("araujofabio2012@gmail.com", email, thanks.encode('utf-8'))
+        server.quit() 
+        
+    return render_template('index.html')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,101 @@
+var isname = document.querySelector('#name');
+var email = document.querySelector('#email');
+var message = document.querySelector('#message');
+var btnSend = document.getElementById('send');
+
+var okName = false;
+var okEmail= false;
+var okMsg = false;
+btnSend.disabled = true;
+
+
+isname.addEventListener('keyup', () =>{
+
+    if(isname.value.length < 3){
+        isname.style.borderColor = 'red';
+        okName = false;
+    }else{
+        isname.style.borderColor = 'green';
+        okName = true;
+    }
+
+    if(okName && okEmail && okMsg){
+        btnSend.disabled = false;
+    }else{
+        btnSend.disabled = true;
+    }
+
+});
+
+email.addEventListener('keyup', () =>{
+    var check=/@[w-]+./;
+    if(email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1){  
+        email.style.borderColor = 'red';
+        
+        okEmail = false;
+    }else{
+        email.style.borderColor = 'green';
+        okEmail = true;
+    }
+
+
+    if(okName && okEmail && okMsg){
+        btnSend.disabled = false;
+    }else{
+        btnSend.disabled = true;
+    }
+
+});
+
+message.addEventListener('keyup', () =>{
+
+    if(message.value.length > 100 || message.value.length < 3){
+        message.style.borderColor = 'red';
+        okMsg = false;
+    }else{
+        message.style.borderColor = 'green';
+        okMsg = true;
+    }
+
+    if (okName && okEmail && okMsg) {
+        btnSend.disabled = false;
+     } else {
+        btnSend.disabled = true;
+     }
+});
+
+btnSend.addEventListener('click', () =>{
+    alert('Mensagem enviada com sucesso!')
+});
+
+
+
+
+
+document.addEventListener("scroll", handleScroll);
+// referencia do butão
+var topBtn = document.querySelector("#btntop");
+
+function handleScroll() {
+  var scrollableHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var visible = 0.2;
+
+  if ((document.documentElement.scrollTop / scrollableHeight ) > visible) {
+    //show button
+    topBtn.style.display = "block";
+  } else {
+    //hide button
+    topBtn.style.display = "none";
+  }
+}
+
+// evento click scrooll top 
+
+topBtn.addEventListener("click", scrollToTop);
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
